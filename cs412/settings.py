632878be4d0c7468f81fe17f519b09b496739ac4
msur2714 <msur2714@bu.edu>
2024-09-23 15:43:33 -0400

    Created quotes app

--- cs412/settings.py
+++ cs412/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-c7^jr7tqdc=)rdehgxkw1w&d_#3l4%_7#ip52)d5@dwy2x2@=s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',
+                 'cs412-cbnlvdyo9-msur2714s-projects.vercel.app', 'localhost']
 
 
 # Application definition
