cc75c790847a3dceaf6ddafd43542a503c5493fd
msur2714 <msur2714@bu.edu>
2024-09-23 18:21:52 -0400

    Created quotes app

--- cs412/settings.py
+++ cs412/settings.py
@@ -80,21 +80,11 @@ WSGI_APPLICATION = "cs412.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://poscd ~/Desktop/django
-tgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 # Password validation
