a8f377a05f177e6677fc33ab9274bea14a0a0aff
msur2714 <msur2714@bu.edu>
2024-10-05 14:21:20 -0400

    Added views and template for mini_fb

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -11,5 +11,5 @@ class ShowAllView(ListView):
     '''The view to show all Profiles'''
 
     model = Profile 
-    template_name = 'mini_fb/show_all.html'
+    template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles' 
