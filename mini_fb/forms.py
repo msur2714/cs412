2967a605c80e971633e552d708250fb012ec5c21
msur2714 <msur2714@bu.edu>
2024-10-14 20:47:35 -0400

    Added views and template for mini_fb

--- mini_fb/forms.py
+++ mini_fb/forms.py
@@ -3,16 +3,6 @@
 from django import forms
 from .models import Profile, StatusMessage
 
-# class CreateProfileForm(forms.ModelForm):
-#     '''A form to add/create a new Profile to the database'''
-
-#     class Meta: 
-#         '''Associate this HTML form with the StatusMessage data model'''
-
-#         model = StatusMessage 
-#         fields = ['message', 'profile']
-
-
 class CreateProfileForm(forms.ModelForm):
     first_name = forms.CharField(label="First Name", required=True)
     last_name = forms.CharField(label="Last Name", required=True)
@@ -27,5 +17,5 @@ class CreateProfileForm(forms.ModelForm):
 class CreateStatusMessageForm(forms.ModelForm):
     class Meta:
         model = StatusMessage
-        fields = ['message']  # Assuming 'message' is a field in your StatusMessage model
+        fields = ['message']  
         labels = {'message': 'Status Message'}
